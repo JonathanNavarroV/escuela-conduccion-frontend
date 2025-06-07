c83f910fa92169e16cb3672119208fa248d4bdcb
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-06-07 15:53:09 -0400

    feat(users): Se separa la tabla en un componente común para reutilizarlo

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -1,51 +1,31 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-import { MatFormField } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { firstValueFrom } from 'rxjs';
 import { ApiResponse } from '../../../core/models/common/api-response.model';
 import { User } from '../../../core/models/users/user.model';
 import { UserService } from '../../../core/services/user.service';
+import { DataTableComponent } from '../../../shared/components/data-table/data-table.component';
 import { CreateUserDialogComponent } from './dialogs/create-user-dialog/create-user-dialog.component';
 
 @Component({
 	selector: 'app-users',
 	standalone: true,
 	imports: [
-		MatFormField,
-		MatInputModule,
 		MatTableModule,
+		MatIconModule,
 		MatMenuModule,
 		MatButtonModule,
-		MatIconModule,
-		MatPaginatorModule,
-		CommonModule,
+		DataTableComponent,
 	],
 	templateUrl: './users.component.html',
 	styleUrl: './users.component.scss',
 })
 export class UsersComponent {
-	users: User[] = [];
-
-	displayedColumns: string[] = ['user', 'email', 'role', 'actions'];
-
-	/**
-	 * Fuente de datos para la tabla de usuarios.
-	 * Se actualiza cuando se reciben los datos desde el backend.
-	 */
-	dataSource = new MatTableDataSource<User>(this.users);
-
-	/**
-	 * Referencia al componente de paginación de Angular Material.
-	 * Se vincula al datasource en `ngAfterViewInit`.
-	 */
-	@ViewChild(MatPaginator) paginator?: MatPaginator;
+	usersDataSource: MatTableDataSource<User> = new MatTableDataSource<User>();
 
 	dialog = inject(MatDialog);
 
@@ -53,18 +33,6 @@ export class UsersComponent {
 
 	ngOnInit(): void {
 		this.getUsers();
-	}
-
-	/**
-	 * Se ejecuta después de que la vista del componente ha sido inicializada.
-	 * Asocia el paginador de Angular Material al dataSource de la tabla para habilitar la paginación.
-	 *
-	 * @returns {void} No retorna ningún valor.
-	 */
-	ngAfterViewInit(): void {
-		if (this.paginator) {
-			this.dataSource.paginator = this.paginator;
-		}
 	}
 
 	/**
@@ -77,7 +45,7 @@ export class UsersComponent {
 		const apiResponse: ApiResponse<User[]> = await firstValueFrom(
 			this.userService.getUsers(),
 		);
-		this.users = apiResponse.data;
+		this.usersDataSource.data = apiResponse.data;
 	}
 
 	/**
@@ -88,13 +56,11 @@ export class UsersComponent {
 	 * @param {Event} event - Evento disparado al modificar el valor del input de búsqueda.
 	 * @returns {Promise<void>} Promesa que se resuelve cuando la lista de usuarios es actualizada.
 	 */
-	async getUsersByFullName(event: Event): Promise<void> {
-		const fullName = (event.target as HTMLInputElement).value;
-
+	async getUsersByFullName(fullName: string): Promise<void> {
 		const apiResponse: ApiResponse<User[]> = await firstValueFrom(
 			this.userService.getUsersByFullName(fullName),
 		);
-		this.users = apiResponse.data;
+		this.usersDataSource.data = apiResponse.data;
 		if (fullName.length === 0) {
 			this.getUsers();
 		}
@@ -104,6 +70,26 @@ export class UsersComponent {
 	 * Abre el diálogo para la creación de un nuevo usuario.
 	 */
 	openCreateUserDialog(): void {
+		this.dialog.open(CreateUserDialogComponent, {
+			minWidth: '720px',
+			width: '720px',
+		});
+	}
+
+	/**
+	 * Abre el diálogo para la creación de un nuevo usuario.
+	 */
+	openUpdateUserDialog(): void {
+		this.dialog.open(CreateUserDialogComponent, {
+			minWidth: '720px',
+			width: '720px',
+		});
+	}
+
+	/**
+	 * Abre el diálogo para la creación de un nuevo usuario.
+	 */
+	openConfirmDeleteDialog(): void {
 		this.dialog.open(CreateUserDialogComponent, {
 			minWidth: '720px',
 			width: '720px',
