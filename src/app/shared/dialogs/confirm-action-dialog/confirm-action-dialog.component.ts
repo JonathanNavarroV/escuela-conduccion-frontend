3001288538b4290a2dc4f5d60aa8da6399438dff
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-06-12 19:49:56 -0400

    feat(confirm-action-dialog): Configuración del dialog confirm-action-dialog

--- src/app/shared/dialogs/confirm-action-dialog/confirm-action-dialog.component.ts
+++ src/app/shared/dialogs/confirm-action-dialog/confirm-action-dialog.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+	MAT_DIALOG_DATA,
+	MatDialogModule,
+	MatDialogRef,
+} from '@angular/material/dialog';
+import { ConfirmDialogData } from '../../../core/models/common/confirm-dialog-data';
 
 @Component({
 	selector: 'app-confirm-action-dialog',
 	standalone: true,
-	imports: [],
+	imports: [MatDialogModule, MatButtonModule],
 	templateUrl: './confirm-action-dialog.component.html',
 	styleUrl: './confirm-action-dialog.component.scss',
 })
-export class ConfirmActionDialogComponent {}
+export class ConfirmActionDialogComponent {
+	private readonly dialogRef = inject(
+		MatDialogRef<ConfirmActionDialogComponent>,
+	);
+	protected readonly data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+
+	protected onConfirmClick(): void {
+		this.dialogRef.close(true);
+	}
+}
